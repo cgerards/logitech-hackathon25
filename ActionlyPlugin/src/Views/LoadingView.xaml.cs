1391dedfcdd6dd0f8c7144b10d847a6b8fc7900f
Christoph <77243803+cgerards@users.noreply.github.com>
2025-11-22 14:22:29 +0100

    Finish Loading Screen concept

--- ActionlyPlugin/src/Views/LoadingView.xaml.cs
+++ ActionlyPlugin/src/Views/LoadingView.xaml.cs
@@ -9,22 +9,9 @@
         public LoadingView()
         {
             InitializeComponent();
-            this.InnerInputTextBox.KeyDown += InnerInputTextBox_KeyDown;
         }
 
-        private void InnerInputTextBox_KeyDown(object sender, KeyEventArgs e)
-        {
-            if (e.Key == Key.Enter)
-            {
-                this.SubmitRequested?.Invoke(this, EventArgs.Empty);
-                e.Handled = true;
-            }
-        }
 
-        public event EventHandler SubmitRequested;
 
-        public string Text => this.InnerInputTextBox?.Text ?? string.Empty;
-
-        public void FocusInput() => this.InnerInputTextBox?.Focus();
     }
 }
