22f6b73dd9f39a0523985207b78dae93fa72b4f2
Samuel Kob <65401077+wizard425@users.noreply.github.com>
2025-11-22 15:02:15 +0100

    Connection with loading screen (#6)

--- ActionlyPlugin/src/Helpers/Models/AIResponse.cs
+++ ActionlyPlugin/src/Helpers/Models/AIResponse.cs
@@ -1,14 +1,8 @@
 ﻿namespace Loupedeck.ActionlyPlugin.Helpers.Models
 {
-    using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Text;
-    using System.Threading.Tasks;
-
-    internal class AIResponse
+    public class AIResponse
     {
-        public string Explanation { get; set; }
-        public string[] Combinations { get; set; }
+        public String Explanation { get; set; }
+        public String[] Combinations { get; set; }
     }
 }
