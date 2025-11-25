a9c9b693c819284c6700b799699dae20b7a210fb
Samuel Kob <samuel.kob@tum.de>
2025-11-25 14:42:48 +0100

    adjusted prompt

--- ActionlyPlugin/src/Helpers/Prompt.cs
+++ ActionlyPlugin/src/Helpers/Prompt.cs
@@ -15,7 +15,7 @@
         "None, LeftButton, RightButton, Break, MiddleButton, XButton1, XButton2, Back, Tab, BackTab, Clear, Return, Shift, Control, Alt, Pause, Caps, Escape, Space, PageUp, PageDown, End, Home, ArrowLeft, ArrowUp, ArrowRight, ArrowDown, PrintScreen, Insert, Delete, Help, Key0, Key1, Key2, Key3, Key4, Key5, Key6, Key7, Key8, Key9, KeyA, KeyB, KeyC, KeyD, KeyE, KeyF, KeyG, KeyH, KeyI, KeyJ, KeyK, KeyL, KeyM, KeyN, KeyO, KeyP, KeyQ, KeyR, KeyS, KeyT, KeyU, KeyV, KeyW, KeyX, KeyY, KeyZ, WindowsLeft, WindowsRight, Apps, Sleep, NumPad0, NumPad1, NumPad2, NumPad3, NumPad4, NumPad5, NumPad6, NumPad7, NumPad8, NumPad9, Multiply, Add, Separator, Subtract, Decimal, Divide, F1, F2, F3, F4, F5, F6, F7, F8, F9, F10, F11, F12, F13, F14, F15, F16, F17, F18, F19, F20, F21, F22, F23, F24, NumLock, ScrollLock, ShiftLeft, ShiftRight, ControlLeft, ControlRight, AltLeft, AltRight, VolumeMute, VolumeDown, VolumeUp, MediaNextTrack, MediaPrevTrack, MediaStop, MediaPlayPause, Oem1, Equals, Comma, Minus, Period, Oem2, Oem3, Oem4, Oem5, Oem6, Oem7, Oem102, Packet\n\n" +
         "For combining KeyCodes use this pattern:\n" +
         "KeyCode1 + KeyCode2 + ... + KeyCodeN\n" +
-        "If the user wants to enter text, represent the text input as a KeyCombination, formatted as String>TextInput<\n" +
+        "If the user wants to enter text, represent the text input as a KeyCombination, formatted as String>TextInput< \n" +
         "Use exactly the keycodes provided before. It is crucial that you do not invent or assume the existence of any other key codes.\n" +
         "If you want to press Letters, the corresponding KeyCodes are KeyA to KeyZ.\n\n" +
         "Follow these rules:\n\n" +
